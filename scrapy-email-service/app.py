b941a79512f93be24f1c2fdc5d4ff04c1f9d333a
Sunny <swastik.ojas@gmail.com>
2026-02-03 06:16:42 -0600

    Updated app.py

--- scrapy-email-service/app.py
+++ scrapy-email-service/app.py
@@ -225,5 +225,5 @@ def find_and_verify():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5002))
     app.run(host='0.0.0.0', port=port, debug=True)
