540b9c0bd78bf32fecb488b8c963897ae261bc3c
Sunny <swastik.ojas@gmail.com>
2026-02-18 14:03:34 -0600

    Updated publishers.py

--- email-scraper-service/publishers.py
+++ email-scraper-service/publishers.py
@@ -3,96 +3,280 @@ PUBLISHERS = [
         "name": "New York Times",
         "rss": "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
         "domain": "nytimes.com",
+        "region": "Northeast",
         "author_fields": ["dc_creator", "byline", "author"]
     },
     {
         "name": "Reuters",
         "rss": "http://feeds.reuters.com/reuters/technologyNews",
         "domain": "reuters.com",
+        "region": "National",
         "author_fields": ["author"]
     },
     {
         "name": "TechCrunch",
         "rss": "https://techcrunch.com/feed/",
         "domain": "techcrunch.com",
+        "region": "West Coast",
         "author_fields": ["dc_creator", "author"]
     },
     {
         "name": "The Verge",
         "rss": "https://www.theverge.com/rss/index.xml",
         "domain": "theverge.com",
+        "region": "Northeast",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "Wired",
         "rss": "https://www.wired.com/feed/rss",
         "domain": "wired.com",
+        "region": "West Coast",
         "author_fields": ["dc_creator", "author"]
     },
     {
         "name": "Ars Technica",
         "rss": "https://feeds.arstechnica.com/arstechnica/index",
         "domain": "arstechnica.com",
+        "region": "National",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "The Wall Street Journal Tech",
         "rss": "https://feeds.a.dj.com/rss/RSSWSJD.xml",
         "domain": "wsj.com",
+        "region": "Northeast",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "CNN Business Tech",
         "rss": "http://rss.cnn.com/rss/cnn_tech.rss",
         "domain": "cnn.com",
+        "region": "Southeast",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "BBC Technology",
         "rss": "http://feeds.bbci.co.uk/news/technology/rss.xml",
         "domain": "bbc.com",
+        "region": "International",
         "author_fields": ["author"]
     },
     {
         "name": "CNET",
         "rss": "https://www.cnet.com/rss/news/",
         "domain": "cnet.com",
+        "region": "West Coast",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "VentureBeat",
         "rss": "https://venturebeat.com/feed/",
         "domain": "venturebeat.com",
+        "region": "West Coast",
         "author_fields": ["dc_creator", "author"]
     },
     {
         "name": "Engadget",
         "rss": "https://www.engadget.com/rss.xml",
         "domain": "engadget.com",
+        "region": "National",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "ZDNet",
         "rss": "https://www.zdnet.com/news/rss.xml",
         "domain": "zdnet.com",
+        "region": "National",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "Forbes Technology",
         "rss": "https://www.forbes.com/innovation/feed/",
         "domain": "forbes.com",
+        "region": "National",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "Fast Company Technology",
         "rss": "https://www.fastcompany.com/technology/rss",
         "domain": "fastcompany.com",
+        "region": "Northeast",
         "author_fields": ["author", "dc_creator"]
     },
     {
         "name": "MIT Technology Review",
         "rss": "https://www.technologyreview.com/feed/",
         "domain": "technologyreview.com",
+        "region": "Northeast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Boston Globe Tech",
+        "rss": "https://www.bostonglobe.com/business/technology/?outputType=rss",
+        "domain": "bostonglobe.com",
+        "region": "Northeast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Philadelphia Inquirer Business",
+        "rss": "https://www.inquirer.com/business/arc.xml",
+        "domain": "inquirer.com",
+        "region": "Northeast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Washington Post Tech",
+        "rss": "https://feeds.washingtonpost.com/rss/business/technology",
+        "domain": "washingtonpost.com",
+        "region": "Mid-Atlantic",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "USA Today Tech",
+        "rss": "http://rssfeeds.usatoday.com/usatoday-TechTopStories",
+        "domain": "usatoday.com",
+        "region": "National",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Miami Herald Business",
+        "rss": "https://www.miamiherald.com/news/business/arc.xml",
+        "domain": "miamiherald.com",
+        "region": "Southeast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Atlanta Journal-Constitution Business",
+        "rss": "https://www.ajc.com/business/?outputType=rss",
+        "domain": "ajc.com",
+        "region": "Southeast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Charlotte Observer Business",
+        "rss": "https://www.charlotteobserver.com/news/business/arc.xml",
+        "domain": "charlotteobserver.com",
+        "region": "Southeast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Dallas Morning News Business",
+        "rss": "https://www.dallasnews.com/business/rss/",
+        "domain": "dallasnews.com",
+        "region": "Southwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Houston Chronicle Business",
+        "rss": "https://www.houstonchronicle.com/business/rss/",
+        "domain": "houstonchronicle.com",
+        "region": "Southwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Austin American-Statesman Tech",
+        "rss": "https://www.statesman.com/business/?outputType=rss",
+        "domain": "statesman.com",
+        "region": "Southwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Chicago Tribune Business",
+        "rss": "https://www.chicagotribune.com/business/?outputType=rss",
+        "domain": "chicagotribune.com",
+        "region": "Midwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Detroit Free Press Business",
+        "rss": "https://www.freep.com/business/?outputType=rss",
+        "domain": "freep.com",
+        "region": "Midwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Minneapolis Star Tribune Business",
+        "rss": "https://www.startribune.com/business/?rss_full=1",
+        "domain": "startribune.com",
+        "region": "Midwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Denver Post Business",
+        "rss": "https://www.denverpost.com/business/feed/",
+        "domain": "denverpost.com",
+        "region": "Mountain West",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Arizona Republic Business",
+        "rss": "https://www.azcentral.com/business/?outputType=rss",
+        "domain": "azcentral.com",
+        "region": "Southwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Seattle Times Business",
+        "rss": "https://www.seattletimes.com/business/feed/",
+        "domain": "seattletimes.com",
+        "region": "Pacific Northwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Portland Oregonian Business",
+        "rss": "https://www.oregonlive.com/business/index.ssf/rss.xml",
+        "domain": "oregonlive.com",
+        "region": "Pacific Northwest",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Los Angeles Times Business",
+        "rss": "https://www.latimes.com/business/rss2.0.xml",
+        "domain": "latimes.com",
+        "region": "West Coast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "San Francisco Chronicle Business",
+        "rss": "https://www.sfchronicle.com/business/feed/",
+        "domain": "sfchronicle.com",
+        "region": "West Coast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "San Diego Union-Tribune Business",
+        "rss": "https://www.sandiegouniontribune.com/business/?outputType=rss",
+        "domain": "sandiegouniontribune.com",
+        "region": "West Coast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Sacramento Bee Business",
+        "rss": "https://www.sacbee.com/news/business/arc.xml",
+        "domain": "sacbee.com",
+        "region": "West Coast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Silicon Valley Business Journal",
+        "rss": "https://www.bizjournals.com/sanjose/feeds/rss/news.xml",
+        "domain": "bizjournals.com/sanjose",
+        "region": "West Coast",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "Mashable",
+        "rss": "https://mashable.com/feeds/rss/all",
+        "domain": "mashable.com",
+        "region": "National",
+        "author_fields": ["author", "dc_creator"]
+    },
+    {
+        "name": "The Information",
+        "rss": "https://www.theinformation.com/feed",
+        "domain": "theinformation.com",
+        "region": "West Coast",
         "author_fields": ["author", "dc_creator"]
     }
 ]
